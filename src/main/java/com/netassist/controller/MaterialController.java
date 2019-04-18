aaba1998297bd4e367e51e6ddcbcea99d16453f1
androidheng <19930425zh>
2019-04-18 18:05:09 +0800

    Signed-off-by: androidheng <19930425zh>

--- src/main/java/com/netassist/controller/MaterialController.java
+++ src/main/java/com/netassist/controller/MaterialController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -53,43 +54,78 @@ public class MaterialController {
 	 * @return
 	 */
 	@RequestMapping("/add")
-	public Result add( TbMaterial material,MultipartFile file,HttpServletRequest request){
+	public Result add( TbMaterial material,@RequestParam("files") MultipartFile[] files,HttpServletRequest request){
 		try {
-			if (file!=null) {// 判断上传的文件是否为空
-	            String path=null;// 文件路径
-	            String type=null;// 文件类型
-	            String fileName=file.getOriginalFilename();// 文件原名称
-	            System.out.println("上传的文件原名称:"+fileName);
-	            // 判断文件类型
-	            type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
-	            if (type!=null) {// 判断文件类型是否为空
-	                if ("GIF".equals(type.toUpperCase())||"PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {
-	                    // 项目在容器中实际发布运行的根路径
-	                    String realPath=request.getSession().getServletContext().getRealPath("upload/");
-	                    // 自定义的文件名称
-	                    String trueFileName=String.valueOf(System.currentTimeMillis())+fileName.substring(fileName.indexOf("."));
-	                    material.setLogo("upload/"+trueFileName);
-	                    // 设置存放图片文件的路径
-	                    path=realPath+File.separator+trueFileName;
-	                    System.out.println("存放图片文件的路径:"+path);
-	                    // 转存文件到指定的路径
-	                    try {
-							file.transferTo(new File(path));
-						} catch (IllegalStateException e) {
-							e.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-	                    System.out.println("文件成功上传到指定目录下");
-	                }else {
-	                    System.out.println("不是我们想要的文件类型,请按要求重新上传");
-	                    return null;
-	                }
-	            }else {
-	                System.out.println("文件类型为空");
-	                return null;
-	            }
+			if (files!=null) {// 判断上传的文件是否为空
+				System.out.println( files.length);
+				for(int i=0;i< files.length;i++) {
+					MultipartFile file=files[i];
+					  String path=null;// 文件路径
+			            String type=null;// 文件类型
+			            String fileName=file.getOriginalFilename();// 文件原名称
+			            System.out.println("上传的文件原名称:"+fileName);
+			            // 判断文件类型
+			            type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+			            if (type!=null) {// 判断文件类型是否为空
+			            	if(i==0) {
+			            		if ("GIF".equals(type.toUpperCase())||"PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {
+				                    // 项目在容器中实际发布运行的根路径
+				                    String realPath=request.getSession().getServletContext().getRealPath("upload/");
+				                    // 自定义的文件名称
+				                    String trueFileName=String.valueOf(System.currentTimeMillis())+fileName.substring(fileName.indexOf("."));
+				                	   material.setLogo("upload/"+trueFileName);
+				                    
+				                    // 设置存放图片文件的路径
+				                    path=realPath+File.separator+trueFileName;
+				                    System.out.println("存放图片文件的路径:"+path);
+				                    // 转存文件到指定的路径
+				                    try {
+										file.transferTo(new File(path));
+									} catch (IllegalStateException e) {
+										e.printStackTrace();
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+				                    System.out.println("文件成功上传到指定目录下");
+				                }else {
+				                    System.out.println("不是我们想要的文件类型,请按要求重新上传");
+				                    return new Result(true, "不是我们想要的文件类型,请按要求重新上传");
+				                }
+			            	}else {
+			            		if ("TXT".equals(type.toUpperCase())||"PDF".equals(type.toUpperCase())||"DOC".equals(type.toUpperCase())||"DOCX".equals(type.toUpperCase())) {
+				                    // 项目在容器中实际发布运行的根路径
+				                    String realPath=request.getSession().getServletContext().getRealPath("upload/");
+				                    // 自定义的文件名称
+				                    String trueFileName=String.valueOf(System.currentTimeMillis())+fileName.substring(fileName.indexOf("."));
+				                	   material.setUrl("upload/"+trueFileName);
+				                    
+				                    // 设置存放图片文件的路径
+				                    path=realPath+File.separator+trueFileName;
+				                    System.out.println("存放图片文件的路径:"+path);
+				                    // 转存文件到指定的路径
+				                    try {
+										file.transferTo(new File(path));
+									} catch (IllegalStateException e) {
+										e.printStackTrace();
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+				                    System.out.println("文件成功上传到指定目录下");
+				                }else {
+				                    System.out.println("不是我们想要的文件类型,请按要求重新上传");
+				                    return new Result(true, "不是我们想要的文件类型,请按要求重新上传");
+				                }
+			            	}
+			                
+			            }else {
+			                System.out.println("文件类型为空");
+			              
+			                return null;
+			            }
+				}
+	          
 			
 			} 
 			materialService.add(material);
